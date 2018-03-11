ecd1cc2e0ed4b03777b7b8e4840e113ee1baae51
armogidec <chudak_93@mail.ru>
2018-03-12 00:40:32 +0500

    day4

--- day4/script.hs
+++ day4/script.hs
@@ -12,10 +12,11 @@ parse line = (name, checksum, id) where
     checksum = last params =~ "([a-z]+)" :: String
     id = read (last params =~ "([0-9]+)" :: String) :: Int
 
+resultate (name, checksum, id) = id
 
 compute1 input = result where
     rooms = map parse (lines input)
-    result = rooms
+    result = map resultate rooms
 
 compute2 :: String -> String
 compute2 input = result where
