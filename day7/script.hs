f3c81e076640054a0bb67c676e12fb8c1e4bf3c9
armogidec <chudak_93@mail.ru>
2018-03-16 22:35:01 +0500

    7th day script

--- day7/script.hs
+++ day7/script.hs
@@ -1,14 +1,52 @@
+import Data.Function ((&))
 import Data.List.Split
 import System.Environment
 
-parse line = concatMap (splitOn "]") (splitOn "[" line)
+everyEven (x:y:xs) = x : everyEven xs
+everyEven (x:_) = [x]
+everyEven _ = []
 
--- compute1 :: String -> String
-compute1 = parse . head . lines 
+toGroup groups = (evenGroups, oddGroups) where
+    evenGroups = everyEven groups
+    oddGroups = everyEven (tail groups)
 
-compute2 :: String -> String
-compute2 input = result where
-    result = "result2"
+parse = splitOneOf "[]"
+
+isContainAbba group = 
+    case group of
+        a:b:c:d:other -> (a==d && b==c && a/=b) || isContainAbba (tail group)
+        _ -> False
+
+isSupportTls (insideGroup, outsideGroup) = any isContainAbba insideGroup && not (any isContainAbba outsideGroup)
+
+compute1 :: String -> Int
+compute1 input = 
+    lines input
+    & map parse
+    & map toGroup
+    & filter isSupportTls
+    & length
+
+findAbs chars =
+    case chars of
+        a:b:c:rest ->
+            if (a /= b) && (a == c) then
+                (a, b) : findAbs (tail chars)
+            else findAbs(tail chars)
+        _ -> [] 
+
+isSupportSsl (insideGroup, outsideGroup) = 
+    let abs = outsideGroup & concatMap findAbs;
+        bas = insideGroup & concatMap findAbs & map (\(x,y) -> (y,x))
+    in any (`elem` bas) abs
+
+compute2 :: String -> Int
+compute2 input = 
+    lines input
+    & map parse
+    & map toGroup
+    & filter isSupportSsl
+    & length
     
 main :: IO()
 main = do
