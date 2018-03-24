e34aec626d873847b5a8d3c1cb07451d56f7359e
armogidec <chudak_93@mail.ru>
2018-03-24 14:40:23 +0500

    day 10....fail

---
+++ day10/script.hs
@@ -0,0 +1,24 @@
+import Data.Function ((&))
+import System.Environment
+
+compute1 :: String -> String
+compute1 input = result where
+    result = "result1"
+
+compute2 :: String -> String
+compute2 input = result where
+    result = "result2"
+    
+main :: IO()
+main = do
+    input <- getText
+    let result1 = compute1 input;
+        result2 = compute2 input
+    print result1
+    print result2
+
+getText :: IO String
+getText = do
+    args <- getArgs
+    let path = head args
+    readFile path
